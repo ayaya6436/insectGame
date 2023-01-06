6cf70bfed51bed19dc5ca451f0a38c920a97b854
ayaya <ayaya6436@gmail.com>
2023-01-06 23:09:07 +0100

    last commit

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
+//select elements
 const screens = document.querySelectorAll('.screen');
-
 const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
 
 const start_btn = document.getElementById('start-btn');
 
 const timeEl = document.getElementById('time');
 const scoreEl = document.getElementById('score');
-const messageWinner = document.getElementById('messageWinner');
-
-messageLooser = document.getElementById('messageLooser');
+const message = document.getElementById('message');
 
 
+const replay_btn = document.getElementById('replay-btn');
 
 const game_container = document.getElementById('game-container');
 
-let seconds =20;
+let seconds = 0;
 let score = 0;
 let selected_insect = {};
-var f , intervalId;
+let interval;
+
 
 start_btn.addEventListener('click', () => screens[0].classList.add('up'));
 
@@ -29,55 +29,32 @@ choose_insect_btns.forEach(btn => {
         selected_insect = { src, alt };
         screens[1].classList.add('up');
         setTimeout(createInsect, 1000);
-        startgame();
-        
-        
+        startGame();
+
+
     })
 })
+//funntion start game
+function startGame() {
+  interval=  setInterval(increaseTime, 1000);
+}
 
-
-function startgame() {
-    f = 10; 
-    clearInterval(intervalId); 
-    intervalId = setInterval(increaseTime, 1000);
-    displayTimeCount();
-  }
-  
-  function increaseTime() {
-    f --;
-    
-    displayTimeCount();
-    if (f <= 0 ) {
-      clearInterval(intervalId);
-      messageLooser.classList.add('visible');
+//function increaseTime
+function increaseTime() {
+    let m = Math.floor(seconds / 60);
+    let s = seconds % 60;
+    m = m < 10 ? `0${m}` : m;
+    s = s < 10 ? `0${s}` : s;
+    timeEl.innerHTML= `Time: ${m}:${s}`;
+    seconds++;
+    if(seconds > 20){
+    replay();
 
     }
+
    
-
-  }
-  
-  function displayTimeCount() {
-    timeEl.innerHTML = `Time:${f}`;
 }
-// function startGame() {
-//     setInterval(increaseTime, 1000);
-// }
-
-// function increaseTime() {
-//     // let m = Math.floor(seconds / 60);
-//     let s = seconds % 60;
-//     // m = m < 10 ? `0${m}` : m;
-//     s = s < 10 ? `0${s}` : s;
-//     timeEl.innerHTML = `Time:${s}`;
-
-// while (seconds>10){
-//     seconds--;
-// }
-
-// }
-
-
-
+//function create insect
 function createInsect() {
     const insect = document.createElement('div');
     insect.classList.add('insect');
@@ -85,12 +62,12 @@ function createInsect() {
     insect.style.top = `${y}px`;
     insect.style.left = `${x}px`;
     insect.innerHTML = `<img src="${selected_insect.src}" alt="${selected_insect.alt}"
-    style=" transform: rotate(${Math.random() * 360}deg)"/>`
+    style=" transform: rotate(${Math.random() * 360 }deg )"/>`
     insect.addEventListener('click', catchInsect);
     game_container.appendChild(insect);
 }
 
-
+//function randome location 
 function getRandomLocation() {
     const width = window.innerWidth;
     const height = window.innerHeight;
@@ -99,27 +76,45 @@ function getRandomLocation() {
     return { x, y }
 }
 
+//function catch insect
 function catchInsect() {
     increaseScore();
     this.classList.add('caught');
     setTimeout(() => this.remove(), 2000);
     addInsects();
 }
-
+//function addInsects
 function addInsects() {
     setTimeout(createInsect, 1000);
     setTimeout(createInsect, 1500);
 }
 
-
+// function increseScore
 function increaseScore() {
     score++;
-     if(score>=5) {
-        messageWinner.classList.add('visible');
-       
+    if (score >=10) {
+        message.classList.add('visible');
     }
+    
 
     scoreEl.innerHTML = `Score: ${score}`;
 
-  
 }
+
+replay_btn.addEventListener('click', () => {
+
+    message.classList.remove('visible');
+    replay();
+})
+// function replay
+function replay() {
+    score = 0;
+    seconds = 0;
+    
+   
+    
+}
+
+
+
+
